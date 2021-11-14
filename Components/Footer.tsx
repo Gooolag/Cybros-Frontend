3d9c31bc733e839cfdfa1d2f5468aad85ebd2450
Ayush Shukla <shukla.ayush0203@gmail.com>
2021-11-14 12:21:37 +0530

    jessus shivi now working peacefully

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -3,7 +3,12 @@ import { ListItem, UnorderedList, HStack } from "@chakra-ui/react";
 
 export default function Home() {
     return (
-        <Box backgroundColor="black" marginTop="2rem" padding="3rem">
+        <Box
+            backgroundColor="black"
+            marginTop="2rem"
+            padding="3rem"
+            width="98.6vw"
+        >
             <Box display="flex" justifyContent="center">
                 This is the foooter
             </Box>
