91207f18a4edc700c14971bc9b5051eb6afdc73e
Ayush Shukla <shukla.ayush0203@gmail.com>
2021-12-18 11:10:22 +0530

    Timepass

--- Components/data.js
+++ Components/data.js
@@ -1,5 +1,26 @@
 // this will store all the data for the homepage timeline and then map and render values one by one
 
-const data = [];
+const data = [
+    {
+        name: "Mayank Talwar",
+        cf: "https://codeforces.com/profile/DreadArceus",
+        img: "https://userpic.codeforces.org/1573203/title/915cb0e1ff3a9fc.jpg",
+    },
+    {
+        name: "Shivral Shivi Somani",
+        cf: "https://codeforces.com/profile/shiviDON",
+        img: "https://userpic.codeforces.org/1590151/title/c19fdb14cbc77baa.jpg",
+    },
+    {
+        name: "Astha Singh",
+        cf: "https://codeforces.com/profile/Gamma1729",
+        img: "https://userpic.codeforces.org/1845909/title/acc31876f0f142ca.jpg",
+    },
+    {
+        name: "Shruti Sharma",
+        cf: "https://codeforces.com/profile/shruti_789",
+        img: "",
+    },
+];
 
 export default data;
--- pages/login.tsx
+++ pages/login.tsx
@@ -2,19 +2,26 @@ import { Box } from "@chakra-ui/layout";
 
 export default function Login() {
     return (
-        <Box>
-            <h1>Login</h1>
-            <form>
-                <label>
-                    Username:
-                    <input type="text" />
-                </label>
-                <label>
-                    Password:
-                    <input type="password" />
-                </label>
-                <button type="submit">Login</button>
-            </form>
+        <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignContent="center"
+        >
+            <Box fontSize="2.5rem">Login</Box>
+            <Box display="flex" flexDirection="column">
+                <form>
+                    <label>
+                        Username:
+                        <input type="text" />
+                    </label>
+                    <label>
+                        Password:
+                        <input type="password" />
+                    </label>
+                    <button type="submit">Login</button>
+                </form>
+            </Box>
         </Box>
     );
 }
